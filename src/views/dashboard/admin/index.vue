14ac253371b06255c54469da3ad1832ba1435c17
Jucyfer <502223422@qq.com>
2020-04-01 01:04:02 +0800

    做了项目定制化汉化，新增了用户管理，将用户-角色-权限两两绑定，拆分用户与权限的挂钩

--- src/views/dashboard/admin/index.vue
+++ src/views/dashboard/admin/index.vue
@@ -28,36 +28,36 @@
     </el-row>
 
     <el-row :gutter="8">
-      <el-col
-        :xs="{span: 24}"
-        :sm="{span: 24}"
-        :md="{span: 24}"
-        :lg="{span: 12}"
-        :xl="{span: 12}"
-        style="padding-right:8px;margin-bottom:30px;"
-      >
-        <transaction-table />
-      </el-col>
-      <el-col
-        :xs="{span: 24}"
-        :sm="{span: 12}"
-        :md="{span: 12}"
-        :lg="{span: 6}"
-        :xl="{span: 6}"
-        style="margin-bottom:30px;"
-      >
-        <todo-list />
-      </el-col>
-      <el-col
-        :xs="{span: 24}"
-        :sm="{span: 12}"
-        :md="{span: 12}"
-        :lg="{span: 6}"
-        :xl="{span: 6}"
-        style="margin-bottom:30px;"
-      >
-        <box-card />
-      </el-col>
+      <!--      <el-col-->
+      <!--        :xs="{span: 24}"-->
+      <!--        :sm="{span: 24}"-->
+      <!--        :md="{span: 24}"-->
+      <!--        :lg="{span: 24}"-->
+      <!--        :xl="{span: 24}"-->
+      <!--        style="padding-right:8px;margin-bottom:30px;"-->
+      <!--      >-->
+      <transaction-table />
+      <!--      </el-col>-->
+      <!--      <el-col-->
+      <!--        :xs="{span: 24}"-->
+      <!--        :sm="{span: 12}"-->
+      <!--        :md="{span: 12}"-->
+      <!--        :lg="{span: 6}"-->
+      <!--        :xl="{span: 6}"-->
+      <!--        style="margin-bottom:30px;"-->
+      <!--      >-->
+      <!--        <todo-list />-->
+      <!--      </el-col>-->
+      <!--      <el-col-->
+      <!--        :xs="{span: 24}"-->
+      <!--        :sm="{span: 12}"-->
+      <!--        :md="{span: 12}"-->
+      <!--        :lg="{span: 6}"-->
+      <!--        :xl="{span: 6}"-->
+      <!--        style="margin-bottom:30px;"-->
+      <!--      >-->
+      <!--        <box-card />-->
+      <!--      </el-col>-->
     </el-row>
   </div>
 </template>
@@ -71,8 +71,8 @@ import RaddarChart from './components/RaddarChart'
 import PieChart from './components/PieChart'
 import BarChart from './components/BarChart'
 import TransactionTable from './components/TransactionTable'
-import TodoList from './components/TodoList'
-import BoxCard from './components/BoxCard'
+// import TodoList from './components/TodoList'
+// import BoxCard from './components/BoxCard'
 
 const lineChartData = {
   newVisitis: {
@@ -103,9 +103,9 @@ export default {
     // MapChart,
     PieChart,
     BarChart,
-    TransactionTable,
-    TodoList,
-    BoxCard
+    TransactionTable
+    // TodoList,
+    // BoxCard
   },
   data() {
     return {
