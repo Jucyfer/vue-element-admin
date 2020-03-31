669047e9ee4acebb8f71aaa57d2b008503e53185
= <502223422@qq.com>
2020-03-31 16:52:47 +0800

    Default Changelist

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+<script>
+// var script = document.createElement('script')
+// // script.type = 'text/javascript'
+// script.src = 'https://api.map.baidu.com/api?v=2.0&ak=xfhhaTThl11qYVrqLZii6w8qE5ggnhrY&__ec_v__=20190126'
+// document.head.appendChild(script)
+export default {
+  name: 'App'
+}
+</script>
---
+++ src/views/redirect/index.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  created() {
+    const { params, query } = this.$route
+    console.log('测试:' + this.$route.params.path)
+    console.log(params)
+    console.log(query)
+    const { path } = params
+    this.$router.replace({ path: '/' + path, query })
+  },
+  render: function(h) {
+    return h() // avoid warning message
+  }
+}
+</script>
